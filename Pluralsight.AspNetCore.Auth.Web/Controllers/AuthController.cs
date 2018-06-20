516e10f8a7f8c7a4171c02d1a8aac2a47f40df4d
APAC\kham441 <maulikk2000@gmail.com>
2018-06-20 23:03:42 +1000

    Add project files.

---
+++ Pluralsight.AspNetCore.Auth.Web/Controllers/AuthController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCore.Auth.Web.Models;
+using AspNetCore.Auth.Web.Services;
+
+namespace AspNetCore.Auth.Web.Controllers
+{
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [Route("signin")]
+        public IActionResult SignIn()
+        {
+            return View(new SignInModel());
+        }
+
+        [Route("signin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignIn(SignInModel model, string returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                User user;
+                if(await _userService.ValidateCredentials(model.Username, model.Password, out user))
+                {
+                    await SignInUser(user.Username);
+                    if(returnUrl != null)
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model); //this model contains the error message as we have created on the view
+        }
+
+        [Route("signup")]
+        public IActionResult SignUp()
+        {
+            return View(new SignUpModel());
+        }
+
+        [Route("signout")]
+        [HttpPost]
+        public async Task<IActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
+        private async Task SignInUser(string username)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,username),
+                new Claim("UserName",username),
+                new Claim(ClaimTypes.SerialNumber,username)
+            };
+
+            var identity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme,"name",null);
+            var principal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(principal);
+        }
+
+
+    }
+}
---
+++ Pluralsight.AspNetCore.Auth.Web/Services/DummyUserService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BCrypt;
+
+namespace AspNetCore.Auth.Web.Services
+{
+    public class DummyUserService : IUserService
+    {
+        private IDictionary<string, (string PasswordHash, User User)> _users =
+            new Dictionary<string, (string PasswordHash, User User)>();
+
+        public DummyUserService(IDictionary<string, string> users)
+        {
+            foreach (var user in users)
+            {
+                var salt = BCryptHelper.GenerateSalt();
+                _users.Add(user.Key.ToLower(), (BCryptHelper.HashPassword(user.Value, salt), new User(user.Key)));
+            }
+        }
+
+        public Task<bool> AddUser(string username, string password)
+        {
+            if (_users.ContainsKey(username.ToLower()))
+            {
+                return Task.FromResult(false);
+            }
+            string salt = BCryptHelper.GenerateSalt();
+            _users.Add(username.ToLower(), (BCryptHelper.HashPassword(password, salt), new User(username)));
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> ValidateCredentials(string username, string password, out User user)
+        {
+            user = null;
+            var key = username.ToLower();
+            if (_users.ContainsKey(key))
+            {
+                var hash = _users[key].PasswordHash;
+                if (BCryptHelper.CheckPassword(password, hash))
+                {
+                    user = _users[key].User;
+                    return Task.FromResult(true);
+                }
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(false);
+        }
+    }
+}
